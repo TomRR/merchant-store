104a8caad77680a3515c13b368671f64bc62b23a
TomRR <resingtr@gmail.com>
2021-07-04 18:37:06 +0200

    add healthchecks ready for requests and alive

--- MerchantStoreApp/MerchantStoreApi/Startup.cs
+++ MerchantStoreApp/MerchantStoreApi/Startup.cs
@@ -10,9 +10,13 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
+using System.Text.Json;
 using System.Threading.Tasks;
 using MerchantStoreApi.Repositories;
 using MerchantStoreApi.Settings;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -56,7 +60,8 @@ namespace MerchantStoreApi
             services.AddHealthChecks()
                 .AddMongoDb(mongoDbSettingssettings.ConnectionString, 
                     name: "mongodb",
-                    timeout: TimeSpan.FromSeconds(3));
+                    timeout: TimeSpan.FromSeconds(3),
+                    tags: new []{ "ready" });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,7 +83,34 @@ namespace MerchantStoreApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health");
+                // checks if db is ready to take request
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = (check) => check.Tags.Contains("ready"),
+                    ResponseWriter = async (context, report) =>
+                    {
+                        var result = JsonSerializer.Serialize(
+                            new
+                            {
+                                status = report.Status.ToString(),
+                                checks = report.Entries.Select(entry => new
+                                {
+                                    name = entry.Key,
+                                    status = entry.Value.Status.ToString(),
+                                    exception = entry.Value.Exception != null ? entry.Value.Exception.Message : "none",
+                                    duration = entry.Value.Duration.ToString()
+                                })
+                            }
+                        );
+                        context.Response.ContentType = MediaTypeNames.Application.Json;
+                        await context.Response.WriteAsync(result);
+                    }
+                });   
+                // checks if the service is up and running - service = api
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions
+                {
+                    Predicate = (_) => false
+                });
             });
         }
     }
