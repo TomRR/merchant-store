f64f9bcf5702e84a749b3e486de05ca28c874a6d
TomRR <resingtr@gmail.com>
2021-07-06 18:56:24 +0200

    docker

--- MerchantStoreApp/MerchantStoreApi/Startup.cs
+++ MerchantStoreApp/MerchantStoreApi/Startup.cs
@@ -74,7 +74,10 @@ namespace MerchantStoreApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MerchantStoreApi v1"));
             }
 
-            app.UseHttpsRedirection();
+            if (env.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
